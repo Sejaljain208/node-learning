96acd60c4beee40abda837d457929f363388fe04
Sejal Jain <sejal6249@gmail.com>
2026-08-21 00:19:29 +0530

    Add Node.js basics introduction

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+// lecture 1 : basic code of node , how to run : node filename 
+
+const amount =12
+
+if(amount<10){
+    console.log('number is smaller than 10 -> ' , amount)
+}
+else{
+    console.log('number is greater than 10 -> ',amount)
+}
+
+console.log(`\n this is my first program...!! `)
