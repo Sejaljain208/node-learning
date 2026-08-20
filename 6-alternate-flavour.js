9024cf7abe6b7e79f92e51be227f4a1290d16fb9
Sejal Jain <sejal6249@gmail.com>
2026-08-21 00:25:31 +0530

    learned alternate methods to export module

---
+++ 6-alternate-flavour.js
@@ -0,0 +1,15 @@
+// lecture 4 :  here we cover alternate way to export module 
+
+module.exports.friuts =["apple","banana","mango"]
+
+const person ={
+    name1 : "bob",
+    age1 : 20,
+    city1 : "indore",
+
+    name2 : "alice",
+    age2 : 22,
+    city2 : "bhopal",
+}
+
+module.exports.doublePerson =person      //access in 3-module.js
